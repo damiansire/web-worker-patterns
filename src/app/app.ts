194e982e8d43b3131e7a998212c621e362ff91be
Damian Sire <damiansire@gmail.com>
2025-11-02 12:45:39 -0600

    Refactor App Layout and Styles for Improved Structure and Responsiveness
    - Removed the container div to streamline the app layout, enhancing the overall structure.
    - Updated SCSS to reset margin and padding for html and body, ensuring a cleaner layout.
    - Adjusted sidebar styles for better height management and added custom scrollbar styles for improved user experience.
    - Simplified TypeScript imports by removing unused components, focusing on essential layout elements.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/layout/header/header.component';
 import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
-import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true
